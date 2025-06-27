2b85b83b10d44125465a59526346db0e659efeba
CocoHuang111 <2893327218@qq.com>
2025-06-27 21:54:41 +0800

    Add files via upload

---
+++ rolltailedrockfish/mainwindow.h
@@ -0,0 +1,58 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QWidget>
+#include <QPixmap>
+#include <QPushButton>
+#include <QResizeEvent>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    Ui::MainWindow *ui;
+    QPixmap background;
+    int current_page;
+    void setpage1(QWidget*);
+    void setpage2(QWidget*);
+    void setpage3(QWidget*);
+    void setpage4(QWidget*);
+    void setpage5(QWidget*);
+    void setpage6(QWidget*);
+    /*
+    void setpage6();
+    void setpage7();
+*/
+};
+
+class ResizeButton : public QPushButton {
+    Q_OBJECT
+public:
+    explicit ResizeButton(const QString& text, QWidget *parent = nullptr)
+        : QPushButton(text, parent) {}
+
+protected:
+    void resizeEvent(QResizeEvent *event) override {
+        QPushButton::resizeEvent(event);
+        int newFontSize = event->size().height()/4.5 ;
+        QFont f = font();
+        f.setPointSize(newFontSize);
+        setFont(f);
+    }
+};
+
+#endif // MAINWINDOW_H
