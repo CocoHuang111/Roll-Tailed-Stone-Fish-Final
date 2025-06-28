cfb2e9675fd3495d1dc8582e9ebc5f17f4967014
CocoHuang111 <2893327218@qq.com>
2025-06-28 11:46:54 +0800

    Add files via upload

---
+++ rolltailedrockfish/announcementdialog.cpp
@@ -0,0 +1,27 @@
+#include "announcementdialog.h"
+#include <QLineEdit>
+#include <QLabel>
+#include <QPushButton>
+#include <QFormLayout>
+#include <QVBoxLayout>
+
+AnnouncementDialog::AnnouncementDialog(QWidget *parent) : QDialog(parent)  {
+    setWindowTitle("发布新公告");
+
+    // 创建控件
+    QLabel *title = new QLabel("请填写公告内容～");
+    QLineEdit *AnnouncementEdit = new QLineEdit;
+    QPushButton *loginBtn = new QPushButton("确认");
+
+    // 连接槽函数
+    connect(loginBtn, &QPushButton::clicked, this, &QDialog::accept);
+
+    // 使用布局
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(title);
+    mainLayout->addWidget(AnnouncementEdit);
+    mainLayout->addWidget(loginBtn);
+
+    setLayout(mainLayout);
+    setFixedSize(400, 200);  // 可选：设置窗口大小
+}
---
+++ rolltailedrockfish/announcementdialog.h
@@ -0,0 +1,12 @@
+#ifndef ANNOUNCEMENTDIALOG_H
+#define ANNOUNCEMENTDIALOG_H
+
+#include <QDialog>
+
+class AnnouncementDialog : public QDialog
+{
+public:
+    AnnouncementDialog(QWidget *parent = nullptr);
+};
+
+#endif // ANNOUNCEMENTDIALOG_H
