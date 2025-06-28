7ad4cfe8d3895835887939f9dccf548e026aa69f
CocoHuang111 <2893327218@qq.com>
2025-06-28 09:13:21 +0800

    Update registerdialog.cpp

--- rolltailedrockfish/registerdialog.cpp
+++ rolltailedrockfish/registerdialog.cpp
@@ -11,14 +11,14 @@ RegisterDialog::RegisterDialog(QWidget *parent) : QDialog(parent) {
     // 创建控件
     QLabel *title = new QLabel("欢迎注册翻尾石鱼！为了保证交易的顺利进行，请完成以下必要信息的填写！");
     QLineEdit *usernameEdit = new QLineEdit;
-    usernameEdit->setPlaceholderText("请输入用户名");
+    //usernameEdit->setPlaceholderText("请输入用户名");
     QLineEdit *passwordEdit = new QLineEdit;
-    passwordEdit->setPlaceholderText("请输入密码");
+    //passwordEdit->setPlaceholderText("请输入密码");
     passwordEdit->setEchoMode(QLineEdit::Password);
     QLineEdit *ContactEdit = new QLineEdit;
-    ContactEdit->setPlaceholderText("请输入联系方式");
+    //ContactEdit->setPlaceholderText("请输入联系方式");
     QLineEdit *addressEdit = new QLineEdit;
-    usernameEdit->setPlaceholderText("请输入常用交易地址");
+    //usernameEdit->setPlaceholderText("请输入常用交易地址");
 
     QPushButton *loginBtn = new QPushButton("确认");
 
@@ -38,5 +38,5 @@ RegisterDialog::RegisterDialog(QWidget *parent) : QDialog(parent) {
     mainLayout->addWidget(loginBtn);
 
     setLayout(mainLayout);
-    setFixedSize(300, 200);  // 可选：设置窗口大小
+    setFixedSize(400, 200);  // 可选：设置窗口大小
 }
