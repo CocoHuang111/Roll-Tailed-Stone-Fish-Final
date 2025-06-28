df4e5c3adfda047c5afc72d7a93329b8d9b16fcc
hizo-sh <2649331360@qq.com>
2025-06-28 16:17:24 +0800

    Update registerdialog.cpp
    fix welcome

--- rolltailedrockfish/registerdialog.cpp
+++ rolltailedrockfish/registerdialog.cpp
@@ -7,8 +7,7 @@
 
 RegisterDialog::RegisterDialog(QWidget *parent) : QDialog(parent) {
     setWindowTitle("注册");
-    
-    // 初始化成员变量
+    // 初始化成员变量Add commentMore actions
     usernameEdit = new QLineEdit(this);
     usernameEdit->setPlaceholderText("请输入用户名");
 
@@ -19,54 +18,21 @@ RegisterDialog::RegisterDialog(QWidget *parent) : QDialog(parent) {
     contactEdit = new QLineEdit(this);
     contactEdit->setPlaceholderText("请输入联系方式");
 
-    QPushButton *confirmBtn = new QPushButton("确认", this);
+    QPushButton *confirmBtn = new QPushButton("确认注册", this);
 
     // 布局
+    QLabel *title = new QLabel("欢迎注册翻尾石鱼！为了保证交易的顺利进行，请完成以下必要信息的填写！");
     QFormLayout *formLayout = new QFormLayout;
     formLayout->addRow("用户名:", usernameEdit);
     formLayout->addRow("密码:", passwordEdit);
     formLayout->addRow("联系方式:", contactEdit);
 
     QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    mainLayout->addWidget(title);
     mainLayout->addLayout(formLayout);
     mainLayout->addWidget(confirmBtn);
 
-    connect(confirmBtn, &QPushButton::clicked, this, &QDialog::accept);
-    // 创建控件
-    /*
-    QLabel *title = new QLabel("欢迎注册翻尾石鱼！为了保证交易的顺利进行，请完成以下必要信息的填写！");
-    QLineEdit *usernameEdit = new QLineEdit;
-    //usernameEdit->setPlaceholderText("请输入用户名");
-    QLineEdit *passwordEdit = new QLineEdit;
-    //passwordEdit->setPlaceholderText("请输入密码");
-    passwordEdit->setEchoMode(QLineEdit::Password);
-    QLineEdit *ContactEdit = new QLineEdit;
-    //ContactEdit->setPlaceholderText("请输入联系方式");
-    QLineEdit *addressEdit = new QLineEdit;
-    //usernameEdit->setPlaceholderText("请输入常用交易地址");
-
-    QPushButton *loginBtn = new QPushButton("确认");
-
-    // 连接槽函数
-    connect(loginBtn, &QPushButton::clicked, this, &QDialog::accept);
-
-    // 使用布局
-    QFormLayout *formLayout = new QFormLayout;
-    formLayout->addRow("用户名：", usernameEdit);
-    formLayout->addRow("密码：", passwordEdit);
-    formLayout->addRow("联系方式：", ContactEdit);
-    formLayout->addRow("常用交易地址：", addressEdit);
-
-    QVBoxLayout *mainLayout = new QVBoxLayout;
-    mainLayout->addWidget(title);
-    mainLayout->addLayout(formLayout);
-    mainLayout->addWidget(loginBtn);
-
     setLayout(mainLayout);
-    setFixedSize(400, 200);  // 可选：设置窗口大小
 
-    username=usernameEdit->text();
-    password=passwordEdit->text();
-    contact=ContactEdit->text();
-    */
+    connect(confirmBtn, &QPushButton::clicked, this, &QDialog::accept);
 }
