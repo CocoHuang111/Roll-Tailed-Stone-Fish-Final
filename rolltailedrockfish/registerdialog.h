28e288ba8d4744d205b50680c6ce2cddc6c72d79
kangkang0430 <2442965055@qq.com>
2025-06-28 15:47:35 +0800

    Update registerdialog.h

--- rolltailedrockfish/registerdialog.h
+++ rolltailedrockfish/registerdialog.h
@@ -7,9 +7,14 @@ class RegisterDialog : public QDialog
 {Q_OBJECT
 public:
     RegisterDialog(QWidget *parent = nullptr);
-    QString username;
-    QString password;
-    QString contact;
+    QString getUsername() const { return usernameEdit ? usernameEdit->text() : QString(); }
+    QString getPassword() const { return passwordEdit ? passwordEdit->text() : QString(); }
+    QString getContact() const { return contactEdit ? contactEdit->text() : QString(); }
+
+private:
+    QLineEdit *usernameEdit;
+    QLineEdit *passwordEdit;
+    QLineEdit *contactEdit;
 };
 
 #endif // REGISTERDIALOG_H
