25dd08a85a97ec48c88035b318bfc4abf209b0ab
kangkang0430 <2442965055@qq.com>
2025-06-29 15:43:33 +0800

    Update findbookdialog.h

--- rolltailedrockfish/findbookdialog.h
+++ rolltailedrockfish/findbookdialog.h
@@ -8,6 +8,9 @@ class FindbookDialog : public QDialog
 public:
     FindbookDialog(QWidget *parent = nullptr);
     QString bookname;
+    QString getBookname() const { return booknameEdit->text(); }
+private:
+    QLineEdit *booknameEdit; // 需要将编辑框作为成员变量
 };
 
 #endif // FINDBOOKDIALOG_H
