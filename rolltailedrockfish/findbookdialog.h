3d28e6094bf8636703a59edaf6b56a3b9d383472
CocoHuang111 <2893327218@qq.com>
2025-06-29 17:24:59 +0800

    Update findbookdialog.h

--- rolltailedrockfish/findbookdialog.h
+++ rolltailedrockfish/findbookdialog.h
@@ -3,15 +3,38 @@
 
 #include <QDialog>
 #include <QLineEdit>
+#include "Book.h"
 
 class FindbookDialog : public QDialog
 {
 public:
     FindbookDialog(QWidget *parent = nullptr);
-    QString bookname;
-    QString getBookname() const { return booknameEdit->text(); }
+    QStringList newtags;
+    Book getBook() ;
 private:
-    QLineEdit *booknameEdit; // 需要将编辑框作为成员变量
+    QLineEdit *titleEdit; // 需要将编辑框作为成员变量
+    QLineEdit *authorEdit;
+    QLineEdit *priceEdit;
+    QLineEdit *publisherEdit;
+    QLineEdit *descriptionEdit;
+
+    QPushButton *add_tag1;
+    QPushButton *add_tag2;
+    QPushButton *add_tag3;
+    QPushButton *add_tag4;
+    QPushButton *add_tag5;
+    QPushButton *add_tag6;
+    QPushButton *add_tag7;
+    QPushButton *add_tag8;
+    QPushButton *add_tag9;
+
+    QPushButton *add_tag2_1;
+    QPushButton *add_tag2_2;
+    QPushButton *add_tag2_3;
+    QPushButton *add_tag2_4;
+    QPushButton *add_tag2_5;
+
+    QPushButton *loginBtn;
 };
 
 #endif // FINDBOOKDIALOG_H
