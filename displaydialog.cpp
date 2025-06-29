d7529541fed596c62c4ff3653e7d20fd109c3dae
CocoHuang111 <2893327218@qq.com>
2025-06-29 22:17:40 +0800

    Add files via upload

---
+++ displaydialog.cpp
@@ -0,0 +1,36 @@
+#include "displaydialog.h"
+#include <QLabel>
+#include <QFormLayout>
+#include <QPushButton>
+#include <QHBoxLayout>
+
+DisplayDialog::DisplayDialog(Book *book, QWidget *parent)  : QDialog(parent) ,book(book) {
+    setWindowTitle("书本信息");
+    display_title=new QLabel("书名："+book->title);
+    display_author=new QLabel("作者："+book->author);
+    display_publisher=new QLabel("出版商："+book->publisher);
+    display_description=new QLabel("内容简介："+book->description);
+    display_price=new QLabel("售价："+QString::number(book->price));
+    contact_seller=new QPushButton("联系卖家");
+    exit=new QPushButton("取消");
+
+    QVBoxLayout *message_layout=new QVBoxLayout;
+    message_layout->addWidget(display_title);
+    message_layout->addWidget(display_author);
+    message_layout->addWidget(display_publisher);
+    message_layout->addWidget(display_description);
+    message_layout->addWidget(display_price);
+
+    QHBoxLayout *lastline=new QHBoxLayout;
+    lastline->addWidget(exit);
+    lastline->addStretch();
+    lastline->addWidget(contact_seller);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    mainLayout->addLayout(message_layout);
+    mainLayout->addStretch();
+    mainLayout->addLayout(lastline);
+
+    setLayout(mainLayout);
+    setFixedSize(400, 300);
+}
