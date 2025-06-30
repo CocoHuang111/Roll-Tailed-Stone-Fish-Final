83308afb0cc7245a9eeff17267c812d5f627ab73
CocoHuang111 <2893327218@qq.com>
2025-06-30 08:16:03 +0800

    Update displaydialog.h

--- rolltailedrockfish/displaydialog.h
+++ rolltailedrockfish/displaydialog.h
@@ -10,15 +10,15 @@ class DisplayDialog : public QDialog
 {
 public:
     DisplayDialog(Book *book=nullptr, QWidget *parent = nullptr);
+    //DisplayDialog(QWidget *parent = nullptr);
     QPushButton *contact_seller;
     Book *book;
-
-private:
     QLabel *display_title;
     QLabel *display_author;
     QLabel *display_publisher;
     QLabel *display_description;
     QLabel *display_price;
+    QLabel *display_seller;
     QPushButton *exit;
 };
 
