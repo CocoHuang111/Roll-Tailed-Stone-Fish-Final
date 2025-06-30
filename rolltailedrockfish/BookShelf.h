d89c0c76567f092c7a1af84af49c9d2ce88c7782
hizo-sh <2649331360@qq.com>
2025-06-30 11:52:06 +0800

    Update BookShelf.h

--- rolltailedrockfish/BookShelf.h
+++ rolltailedrockfish/BookShelf.h
@@ -22,7 +22,7 @@ public:
     bool addBook(const Book* book);
     bool removeBook(const QString& isbn);
     bool updateBook(const Book* book);
-    Book* findBook(const QString& isbn, std::vector<Book*> books);
+    Book* findBook(const QString& isbn, QList<Book*> books);
 
     // 批量操作
     //QList<Book> findByAuthor(const QString& author) const;
